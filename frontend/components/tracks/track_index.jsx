b40a173db5d525cc482b401a7ebc98da9c2fdf55
Jon Friestedt <jfrieste@gmail.com>
2016-08-07 21:35:53 -0400

    Add intro message to front page

--- frontend/components/tracks/track_index.jsx
+++ frontend/components/tracks/track_index.jsx
@@ -23,13 +23,21 @@ const TrackIndex = React.createClass ({
 
   render () {
     return (
-      <section className="main">
-        <main className="track-index">
-          {this.state.tracks.map( (track) => {
-            return <TrackIndexItem key={track.id} track={track} />;
-          })}
-        </main>
-      </section>
+      <div className="sections-container">
+        <section className="smartypants-intro">
+          <h2>
+            Smartypants is a home for people who know too much about
+            music. Show off your smarts.
+          </h2>
+        </section>
+        <section className="main">
+          <main className="track-index">
+            {this.state.tracks.map( (track) => {
+              return <TrackIndexItem key={track.id} track={track} />;
+            })}
+          </main>
+        </section>
+      </div>
     );
   }
 });
