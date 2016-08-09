47b0e23c02f02a45495ab1ca4daf78e0068e16e2
Jon Friestedt <jfrieste@gmail.com>
2016-08-09 11:45:18 -0400

    Refactor annotation container

--- frontend/components/annotations/annotation_container.jsx
+++ frontend/components/annotations/annotation_container.jsx
@@ -1,16 +1,39 @@
 const React = require('react');
 
 const AnnotationContainer = React.createClass ({
+  getInitialState () {
+    return {
+      annotationButtonRevealed: true,
+      annotationFormRevealed: false
+    };
+  },
+
+  revealAnnotationForm (e) {
+    e.preventDefault();
+    this.setState({
+      annotationButtonRevealed: false,
+      annotationFormRevealed: true
+    });
+  },
 
   render () {
     let containerStyle = {
       top: this.props.annotation.yPosition - 360
     };
+
+    let annotationBeginButton = () => {
+      if (this.state.annotationButtonRevealed) {
+        return (
+          <button className="annotation-begin-button">
+            Start the Smartypants Annotation
+          </button>
+        );
+      }
+    };
+
     return (
       <div className="annotation-container" style={containerStyle}>
-        <button className="annotation-begin-button">
-          Start the Smartypants Annotation
-        </button>
+        {annotationBeginButton()}
       </div>
     );
   }
