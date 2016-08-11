04aa7a19b30d1866cff8308cfd6d2d5bd2f95cc5
Jon Friestedt <jfrieste@gmail.com>
2016-08-11 11:38:21 -0400

    Add validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  author_id        :integer          not null
+#  commentable_type :string           not null
+#  body             :text             not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_id   :integer          not null
+#
+
 class Comment < ActiveRecord::Base
+  validates :author_id, :commentable_type, :commentable_id, :body, presence: true
+
   belongs_to :commentable, polymorphic: true
 end
