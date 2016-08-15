21506e1aab911ccc397ea8d287ca94bb528b5475
Jon Friestedt <jfrieste@gmail.com>
2016-08-15 19:13:26 -0400

    Add production webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/smartypants.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  },
+  devtool: "source-maps"
+};
